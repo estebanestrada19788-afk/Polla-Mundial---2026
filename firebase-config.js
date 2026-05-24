32ae3f1943e31703a305bc84d65509cbcef809ac
estebanestrada19788-afk <esteban.estrada19788@gmail.com>
2026-05-23 22:00:21 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+
+// firebase-config.js
+// Aquí se pegan las credenciales del proyecto Firebase.
+// Este archivo queda listo para la siguiente fase de conexión real con base de datos.
+
+const firebaseConfig = {
+  apiKey: "PEGAR_API_KEY",
+  authDomain: "PEGAR_AUTH_DOMAIN",
+  projectId: "PEGAR_PROJECT_ID",
+  storageBucket: "PEGAR_STORAGE_BUCKET",
+  messagingSenderId: "PEGAR_MESSAGING_SENDER_ID",
+  appId: "PEGAR_APP_ID"
+};
+
+// En la siguiente versión se conectará:
+// firebase.initializeApp(firebaseConfig);
+// const db = firebase.firestore();
